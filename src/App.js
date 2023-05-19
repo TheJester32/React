cc6aa95f93e3d940a111eaae56e5cfe74f5eeea0
TheJester32 <bogdanpopoveducation@gmail.com>
2023-05-19 19:56:18 +0300

    day-3
    day-3 19.05

--- src/App.js
+++ src/App.js
@@ -4,20 +4,38 @@ import Image from "./image"
 import logo from "./img/logo.png"
 
 class App extends React.Component {
-     helpText = "Help text"
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            helpText: "Help text",
+            userData: ""
+        }
+
+        this.inputClick = this.inputClick.bind(this)
+    }
+
+    componentDidUpdate(prevProp){
+        if(this.state.helpText !== "Help")
+        console.log("Some")
+    }
+
     render() {
 
         return (<div className="name">
-            <Header title="Шапка сайта"/>
-            <h1>{this.helpText}</h1>
-            <input placeholder={this.helpText}
+            <Header title="Шапка сайта" />
+            <h1>{this.state.helpText}</h1>
+            <h2>{this.state.userData}</h2>
+            <input placeholder={this.state.helpText}
+                onChange={event => this.setState({ userData: event.target.value })}
                 onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-            <p>{this.helpText === "Help text" ? "Yes" : "No"} </p>
-            <Image image={logo}/>
+            <p>{this.state.helpText === "Help text" ? "Yes" : "No"} </p>
+            <Image image={logo} />
         </div>)
     }
 
     inputClick() {
+        this.setState({ helpText: "Changed" })
         console.log('Clicked')
     }
 
